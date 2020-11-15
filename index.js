6cdfabc81fe9162eb962bb3822ab577842618654
Emilio <egb1982@gmail.com>
2020-11-15 17:34:37 +0100

    Translation enabled

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const { json } = require('body-parser');
 const express = require('express');
 const app = express();
+const i18n = require('express-tongue');
 
+app.use(i18n.localize({ 
+    endpointEnabled: true, 
+    path: __dirname + '/lang', 
+    queryStringEnabled: true
+}));
 
 app.use(express.json());
-
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
-
 app.set('view engine', 'ejs');
 
 app.get('/',(req,res) => {
-    res.status(200).render('main');
+    res.status(200).render('main',{ loc : res.locals.i18n});
 });
 
 const PORT = process.env.PORT || 8080;
