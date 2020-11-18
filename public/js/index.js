3b134afb519b905affd4d1522bae92f71e0dc409
Emilio <egb1982@gmail.com>
2020-11-18 19:08:09 +0100

    correction

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,22 @@
  // when scrolling the navigation bar gets hidden 
  // we show a button to scroll back up to the nav bar
+let lastScroll = 0;
+
  $(document).on('scroll',() => {
 
-    if ($('#iNavigation').offset().top < $(this).scrollTop()) {
-        $('#iMoveUpBtn').fadeIn(1000);
-    } else {
-        $('#iMoveUpBtn').fadeOut(1000);
-    }
+    let st = $(this).scrollTop();
+    console.log(st,lastScroll,$('#iDownloads').offset().top);
+    if (st < lastScroll){
+         // upscroll
+        if ($('#iNavigation').offset().top < st) {
+            $('#iMoveUpBtn').fadeIn(1000);
+        } else {
+            $('#iMoveUpBtn').fadeOut(1000);
+        }
+     } else {
+         //downscroll
+         $('#iMoveUpBtn').fadeOut(1000);
+     }
+
+    lastScroll = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 });
